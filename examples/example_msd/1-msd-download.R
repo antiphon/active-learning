732434e294762f5e09fd876a30362be1e1856a43
Manuel J. A. Eugster <manuel@mjae.net>
2013-11-04 22:22:11 +0200

    MSD

---
+++ examples/example_msd/1-msd-download.R
@@ -0,0 +1,5 @@
+
+download.file("http://static.echonest.com/millionsongsubset_full.tar.gz",
+              "extdata/millionsongsubset_full.tar.gz")
+
+system("tar -xvf extdata/millionsongsubset_full.tar.gz -c extdata/")
