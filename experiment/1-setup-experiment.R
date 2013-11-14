dd74b909ba2b23895f783770c1fe7a462fbaf54b
Manuel J. A. Eugster <manuel@mjae.net>
2013-11-14 23:14:56 +0200

    automated setup of experiment pool

--- experiment/1-setup-experiment.R
+++ experiment/1-setup-experiment.R
@@ -1,5 +1,29 @@
 
-dat <- read.table("../examples/example_dortmund/extdata/Music_features_17_10_2005/music_data_17_10_05.exa",
+### Download "Music Audio Benchmark Data Set": #######################
+
+src <- "http://www-ai.cs.uni-dortmund.de/AUDIO/"
+dest <- "dortmund/"
+
+system(sprintf("wget -r -nd -l1 -P %s %s", src, dest), wait = TRUE)
+
+
+
+### Create musicpool: ################################################
+
+dir.create("musicpool")
+dir.create("sessions")
+
+zip_files <- c("alternative", "blues", "electronic", "folkcountry",
+               "funksoulrnb", "pop", "raphiphop", "rock")  # jazz
+zip_files <- sprintf("%s%s.zip", dest, zip_files)
+
+lapply(zip_files, unzip, exdir = "musicpool")
+
+
+
+### Create pool description: #########################################
+
+dat <- read.table(unz("dortmund/Music_features_17_10_2005.zip", "music_data_17_10_05.exa"),
                   sep = " ", header = FALSE, stringsAsFactors = FALSE)
 
 dat$V34 <- dat$V38 <- dat$V39 <- dat$V42 <- dat$V43 <- dat$V46 <- dat$V47 <- dat$V52 <- NULL
@@ -12,6 +36,7 @@ names(dat) <- c("id", sprintf("feature%s", 1:(ncol(dat) - 2)), "genre")
 ### Remove "jazz" because files are missing:
 
 dat <- subset(dat, genre != "jazz")
+dat$genre <- droplevels(dat$genre)
 
 
 ### Map ids to files:
@@ -29,9 +54,6 @@ dat$file <- w
 
 
 ### Save musicpool description:
-
-dir.create("musicpool")
-dir.create("sessions")
 
 saveRDS(dat, file = "musicpool/description.Rds")
 
